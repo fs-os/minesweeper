9741891883fb5178b8853a1e48bd82fb6acad02b
8dcc <8dcc.git@gmail.com>
2023-04-11 18:55:12 +0200

    Add doxygen comments to minesweeper

--- minesweeper.h
+++ minesweeper.h
@@ -2,8 +2,12 @@
 #ifndef _APPS_MINESWEEPER_H
 #define _APPS_MINESWEEPER_H 1
 
-/* minesweeper_main: entry point of the minesweeper */
+/**
+ * @brief Entry point of the program
+ * @param[in] argc Number of arguments
+ * @param[in] argv String vector with the artuments.
+ * @return Exit code
+ */
 int minesweeper_main(int argc, char** argv);
 
 #endif /* _APPS_MINESWEEPER_H */
-
